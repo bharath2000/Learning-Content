3c01ff38d795ec13c767e63a026a83e815f24ae5
bharath2000 <bharaththakkalapally2000@gmail.com>
2019-04-05 18:10:43 +0530

    text cases are added

--- Phase 2/week 3 - 28 Jan 2019/Exercise2/assignment.py
+++ Phase 2/week 3 - 28 Jan 2019/Exercise2/assignment.py
@@ -4,6 +4,9 @@ from matplotlib import pyplot as plt
 import math
 from scipy import optimize
 import utils
+# define the submission/grader object for this exercise
+grader = utils.Grader()
+
 
 data=np.genfromtxt("Data/ex2data1.txt",delimiter=',')
 X,y =data[:,:2],data[:,2]
@@ -27,6 +30,15 @@ def sigmoid(z):
 	return 1/(1+math.exp(-z))
 
 
+# Test the implementation of sigmoid function here
+z = 0
+g = sigmoid(z)
+
+print('g(', z, ') = ', g)
+
+
+grader[1] = sigmoid
+
 def costFunction(theta,X,y):
 	m=y.size
 	J=0
@@ -36,6 +48,33 @@ def costFunction(theta,X,y):
 		grad+=((sigmoid(np.matmul(theta.T,X[i]))-y[i])*X[i])
 	J/=m;grad/=m
 	return J,grad
+
+
+# Initialize fitting parameters
+initial_theta = np.zeros(n)
+
+cost, grad = costFunction(initial_theta, X, y)
+
+print('Cost at initial theta (zeros): {:.3f}'.format(cost))
+print('Expected cost (approx): 0.693\n')
+
+print('Gradient at initial theta (zeros):')
+print('\t[{:.4f}, {:.4f}, {:.4f}]'.format(*grad))
+print('Expected gradients (approx):\n\t[-0.1000, -12.0092, -11.2628]\n')
+
+# Compute and display cost and gradient with non-zero theta
+test_theta = np.array([-24, 0.2, 0.2])
+cost, grad = costFunction(test_theta, X, y)
+
+print('Cost at test theta: {:.3f}'.format(cost))
+print('Expected cost (approx): 0.218\n')
+
+print('Gradient at test theta:')
+print('\t[{:.3f}, {:.3f}, {:.3f}]'.format(*grad))
+print('Expected gradients (approx):\n\t[0.043, 2.566, 2.647]')
+
+grader[2] = costFunction
+grader[3] = costFunction
 
 
 initial_theta=np.zeros(n)
@@ -61,7 +100,18 @@ def predict(theta,X):
 			p[i]=1
 	return p
 
+#  Predict probability for a student with score 45 on exam 1 
+#  and score 85 on exam 2 
+prob = sigmoid(np.dot([1, 45, 85], theta))
+print('For a student with scores 45 and 85,'
+      'we predict an admission probability of {:.3f}'.format(prob))
+print('Expected value: 0.775 +/- 0.002\n')
 
 p=predict(theta,X)
 print('Train Accuracy: {:.2f} %'.format(np.mean(p == y) * 100))
 print('Expected accuracy (approx): 89.00 %')
+
+
+
+grader[4] = predict
+# grader.grade()
